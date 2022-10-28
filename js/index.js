89a2826168eac6a79b365c92519574b5f0c41608
carol-b-side <116359537+carol-b-side@users.noreply.github.com>
2022-10-28 18:08:18 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -32,42 +32,73 @@ function calculateAll() {
   }
 
   // ITERATION 3
-  let totalValue = document.getElementById("total-value");
+  let totalValue = document.querySelector("#total-value span");
   totalValue.innerHTML = total;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  const target = event.currentTarget; //button clicado
+  const btnParent = event.currentTarget.parentNode; //div do button
+  const product = btnParent.parentNode; //row do produto inteiro
+  const row = product.parentNode; //tabela inteira
 
-  const remove = target.parentNode.parentNode;
-  // removeChild(remove);
-  //Uncaught DOMException: Node.removeChild: The node to be removed is not a child of this node => ???
+  row.removeChild(product);
 
-  if (remove) {
-    remove.parentNode.removeChild(remove);
-  }
-
-  // let product = target;
-  // if (product.parentNode.parentNode) {
-  //   product.parentNode.parentNode.removeChild(product);
-  // }
-  // for (let i = 0; i < product.length; i++) {
-  //   if (product.parentNode) {
-  //     product.parentNode.removeChild(product[i]);
-  //   }
-
-  // }
-
-
+  //calcular o total novamente depois que um item foi apagado
+  calculateAll();
 }
+
+// let product = target;
+// if (product.parentNode.parentNode) {
+//   product.parentNode.parentNode.removeChild(product);
+// }
+// for (let i = 0; i < product.length; i++) {
+//   if (product.parentNode) {
+//     product.parentNode.removeChild(product[i]);
+//   }
+
+// }
+
+
+
 
 // ITERATION 5
 
 function createProduct() {
+  const productName = document.getElementById('input-name').value;
+  console.log(productName); // input name
 
+  const inputPrice = document.getElementById('inputPrice').value;
+  console.log(inputPrice); // input price
+
+  const tbody = document.getElementsByTagName('tbody');
+
+  const tr = document.createElement('tr');
+  tr.classList.add('product');
+
+  const row = `
+  <td class="name">
+  <span>${productName}</span>
+  </td>
+  <td class="price">$<span>${inputPrice}</span></td>
+  <td class="quantity">
+  <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+  <button class="btn btn-remove">Remove</button>
+  </td>
+  `;
+
+  tr.innerHTML = row;
+
+  tbody[0].appendChild(tr);
+
+  const removeBtn = tr.querySelector('.btn-remove');
+
+  removeBtn.addEventListener('click', removeProduct);
 }
 
 window.addEventListener('load', () => {
@@ -79,4 +110,6 @@ window.addEventListener('load', () => {
     removeBtn[i].addEventListener("click", removeProduct);
   }
 
+  const createBtn = document.getElementById('create');
+  createBtn.addEventListener('click', createProduct);
 });
